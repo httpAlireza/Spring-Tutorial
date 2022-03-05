70f7a816c38106532215d153cfea11d6b12b1848
httpAlireza <Alirezakh2866@gmail.com>
2022-03-05 15:18:37 +0330

    create student model

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,71 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+
+@Table(name = "student")
+@Entity(name = "Student")
+public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_seq", sequenceName = "student_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student(Long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
