5e6b5389bcdbde0dc022caf9197f9a8b154d0702
httpAlireza <Alirezakh2866@gmail.com>
2022-03-05 15:55:16 +0330

    StudentService updated

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,37 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    public Student getStudentById (Long id) {
+        return studentRepo.findById(id).orElse(null);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepo.findAll();
+    }
+
+    public void addStudent(Student student) {
+        studentRepo.save(student);
+    }
+
+    public void updateStudent(Student student) {
+        try {
+            studentRepo.save(student);
+        } catch (NullPointerException e) {
+            return;
+        }
+    }
+
+    public void deleteStudentById(Long id){
+        studentRepo.deleteById(id);
+    }
 }
