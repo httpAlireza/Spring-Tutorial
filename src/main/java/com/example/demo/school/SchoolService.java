eff643d05ed9247c2b3f1dff14f61f16608117e4
httpAlireza <Alirezakh2866@gmail.com>
2022-03-08 19:38:23 +0330

    SchoolService added

---
+++ src/main/java/com/example/demo/school/SchoolService.java
@@ -0,0 +1,38 @@
+package com.example.demo.school;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SchoolService {
+
+    @Autowired
+    private SchoolRepo schoolRepo;
+
+    public School getSchoolById (Long id) {
+        return schoolRepo.findById(id).orElse(null);
+    }
+
+    public List<School> getAllSchools () {
+        return schoolRepo.findAll();
+    }
+
+    public void addSchool(School school) {
+        schoolRepo.save(school);
+    }
+
+    public void updateSchool(School school) {
+        try {
+            schoolRepo.save(school);
+        } catch (NullPointerException e) {
+            return;
+        }
+    }
+
+    public void deleteSchoolById(Long id) {
+        schoolRepo.deleteById(id);
+    }
+
+}
